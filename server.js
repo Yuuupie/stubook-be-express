b3ed5a91b63b07c897e64e3ffa7b8848380466b6
Yuuupie <ekeskin98@gmail.com>
2022-01-27 21:13:41 +1100

    Added cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 const db = require('./src/database')
 const express = require('express')
 const sessions = require('express-session')
+const cors = require('cors')
 
 db.sync()
 
 const app = express();
 app.use(express.json())
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 app.use(sessions({ 
   secret: process.env.SECRET_KEY,
   resave: false,
